8eaac9265bbbb1d798b134f1a381e29536701799
Gaurav Sharma <40033910+gauravgs@users.noreply.github.com>
2019-10-01 00:09:47 +0530

    Stack implemented

--- Stack.js
+++ Stack.js
@@ -1,23 +1,18 @@
 class Stack {
   constructor() {
     this.myvar = [];
-    // Push an element to the stack
     this.push = function(e) {
       this.myvar.push(e);
     };
-    // Pop the top  from the stack
     this.pop = function() {
       return this.myvar.pop();
     };
-    // Peek the top from the stack
     this.peek = function() {
       return this.myvar[this.getSize() - 1];
     };
-    // Return the stack size
     this.getSize = function() {
       return this.myvar.length;
     };
-    // Return true if stack is empty
     this.isEmpty = function() {
       return this.myvar.length == 0;
     };
